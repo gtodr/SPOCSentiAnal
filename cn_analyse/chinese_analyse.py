5cc0227e0cf35bc0b3bdc451b4d3db568645ef66
toocin <109913649+gtodr@users.noreply.github.com>
2024-04-19 17:36:56 +0800

    Visualize pos/neg ratio

--- cn_analyse/chinese_analyse.py
+++ cn_analyse/chinese_analyse.py
@@ -1,5 +1,7 @@
 #中文情感分析系统
 from snownlp import SnowNLP
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 # 消极的评论列表
 negativeList = []
@@ -27,10 +29,40 @@ for comment in commentList:
             # 说明是积极评论
             positiveList.append(comment)
 
-print('积极的评论有：%d条，消极的评论有：%d条'% (len(positiveList), len(negativeList)))
-print("积极的评论为：")
-for i in positiveList:
-    print(i)
-print("\n\n\n消极的评论为：")
-for i in negativeList:
-    print(i)
+# 积极评论条数
+pos_comments = len(positiveList)
+# 消极评论条数
+neg_comments = len(negativeList)
+print('积极的评论有：%d条，消极的评论有：%d条'% (pos_comments, neg_comments))
+# print("积极的评论为：")
+# for i in positiveList:
+#     print(i)
+# print("\n\n\n消极的评论为：")
+# for i in negativeList:
+#     print(i)
+
+# 根据评论的数量，计算积极评论和消极评论的比例
+# 评论总数
+total_comments = pos_comments + neg_comments
+# 积极评论比例
+pos_ratio = pos_comments / total_comments
+# 消极评论比例
+neg_ratio = neg_comments / total_comments
+
+# 设置字体为中文字体
+font = FontProperties(fname='/System/Library/Fonts/Supplemental/Songti.ttc')
+# 绘制图表
+plt.figure(figsize=(6, 6))
+labels = ['积极', '消极']
+sizes = [pos_ratio, neg_ratio]
+colors = ['green', 'red']
+explode = (0, 0.1)
+patches, l_text, p_text = plt.pie(sizes, explode=explode, labels=labels, colors=colors, labeldistance=1.1, autopct='%2.0f%%', shadow=False, startangle=90, pctdistance=0.6)
+for t in l_text:
+    t.set_fontproperties(font)
+for t in p_text:
+    t.set_fontproperties(font)
+plt.axis('equal')
+plt.legend(prop=font)
+plt.title('中文评论比例', fontproperties=font)
+plt.show()
--- jp_analyse/Japanese_analyse.py
+++ jp_analyse/Japanese_analyse.py
@@ -1,6 +1,8 @@
 #日语情感分析
-
 from asari.api import Sonar
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
 sonar = Sonar()
 # 消极的评论列表
 negativeList = []
@@ -20,5 +22,36 @@ for comment in commentList:
     else:
         # 说明是积极评论
         positiveList.append(result)
-print('积极的评论有：%d条，消极的评论有：%d条'% (len(positiveList), len(negativeList)))
+
+# 积极评论条数
+pos_comments = len(positiveList)
+# 消极评论条数
+neg_comments = len(negativeList)
+print('积极的评论有：%d条，消极的评论有：%d条'% (pos_comments, neg_comments))
 # print(negativeList)
+
+# 根据评论的数量，计算积极评论和消极评论的比例
+# 评论总数
+total_comments = pos_comments + neg_comments
+# 积极评论比例
+pos_ratio = pos_comments / total_comments
+# 消极评论比例
+neg_ratio = neg_comments / total_comments
+
+# 设置字体为中文字体
+font = FontProperties(fname='/System/Library/Fonts/Supplemental/Songti.ttc')
+# 绘制图表
+plt.figure(figsize=(6, 6))
+labels = ['积极', '消极']
+sizes = [pos_ratio, neg_ratio]
+colors = ['green', 'red']
+explode = (0, 0.1)
+patches, l_text, p_text = plt.pie(sizes, explode=explode, labels=labels, colors=colors, labeldistance=1.1, autopct='%2.0f%%', shadow=False, startangle=90, pctdistance=0.6)
+for t in l_text:
+    t.set_fontproperties(font)
+for t in p_text:
+    t.set_fontproperties(font)
+plt.axis('equal')
+plt.legend(prop=font)
+plt.title('日文评论比例', fontproperties=font)
+plt.show()
